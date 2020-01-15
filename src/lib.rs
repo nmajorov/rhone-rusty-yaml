48dd8d1d68e404794b78670e3dd304db60d28262
Nikolaj Majorov <nikolaj@majorov.biz>
2020-01-15 08:41:20 +0100

    add contributers

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,22 @@
 pub mod rhone_yaml {
-
     use serde::{Deserialize, Serialize};
 
+    /// yaml repesentation of rhone  project
     #[derive(Serialize, Deserialize, Debug)]
     pub struct Project {
+        #[serde(rename = "apiVersion")]
         pub api_version: String,
         pub name: String,
         pub description: String,
         pub version: String,
         pub language: String,
         pub interpreter_version: String,
+        pub contributors: Vec<Contributor>,
     }
 
-  
+    #[derive(Serialize, Deserialize, Debug)]
+    pub struct Contributor {
+        pub name: String,
+        pub email: String,
+    }
 }
--- tests/lib.rs
+++ tests/lib.rs
@@ -1,23 +1,27 @@
 #[cfg(test)]
 mod tests {
 
-use serde_yaml;
-use rust_yaml::rhone_yaml::{Project};
+    use rust_yaml::rhone_yaml::{Contributor, Project};
+    use serde_yaml;
 
     #[test]
     fn it_works() -> Result<(), serde_yaml::Error> {
-
+        let contr1 = Contributor {
+            name: "Nikolaj Majorov".to_string(),
+            email: "nikolaj@majorov.biz".to_string(),
+        };
         let project = Project {
-            api_version: String::from("v1"),
+            api_version: String::from("build.rhone.io/v1"),
             name: String::from("rust-yaml"),
             description: String::from("simple rest project"),
             language: String::from("rust"),
             version: String::from("1.0.1"),
             interpreter_version: String::from("1.39.0"),
+            contributors: vec![contr1],
         };
 
         let s = serde_yaml::to_string(&project)?;
-        println!("yaml {}", s);
+        println!("yaml: \n {}", s);
         Ok(())
     }
 }
