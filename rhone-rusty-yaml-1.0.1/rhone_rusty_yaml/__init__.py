91e0a4ea273d3209027c78f9d5183d68d828e73f
Nikolaj Majorov <nikolaj@majorov.biz>
2020-06-13 13:36:38 +0200

    remove old egg packages

--- rhone-rusty-yaml-1.0.1/rhone_rusty_yaml/__init__.py
+++
@@ -1,8 +0,0 @@
-from .rhone_rusty_yaml import get_json_from_yaml_str
-from .rhone_rusty_yaml import get_json_from_yaml_file
-
-__all__ = ["get_json_from_yaml_file", "get_json_from_yaml_str"]
-
-__version__='1.0.1'
-
-
